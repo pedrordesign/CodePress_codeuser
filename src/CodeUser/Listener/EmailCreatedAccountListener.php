a27cdbd4c12d9a5a2febde607ef647da1d3acc18
Pedro Ramos <pedroramos@sitehosting.com.br>
2016-11-09 08:41:34 -0200

    Criando EmailCreatedAccountListener - parte 1 Criando EmailCreatedAccountListener - parte 2

---
+++ src/CodeUser/Listener/EmailCreatedAccountListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodePress\CodeUser\Listener;
+
+use CodePress\CodeUser\Event\UserCreatedEvent;
+
+class EmailCreatedAccountListener{
+
+    public function handle(UserCreatedEvent $event){
+
+    }
+
+}
